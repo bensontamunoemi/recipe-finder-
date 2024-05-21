1fa3610b0846de557596304e236add3cb178ac03
Benson Tamunoemi <bensontamunoemi@gmail.com>
2024-05-21 01:35:14 -0400

    feat: category, meals, meal details screen

--- lib/screens/meals.dart
+++ lib/screens/meals.dart
@@ -1,11 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:meals/models/meal.dart';
+import 'package:meals/screens/meal_detail.dart';
+import 'package:meals/widgets/meal_item.dart';
 
 class MealsScreen extends StatelessWidget {
   const MealsScreen({super.key, required this.title, required this.meals});
 
   final String title;
   final List<Meal> meals;
+
+  void _selectCategory(BuildContext context, Meal meal) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (ctx) => MealDetailScreen(meal: meal)),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -37,7 +46,12 @@ class MealsScreen extends StatelessWidget {
     } else {
       content = ListView.builder(
         itemCount: meals.length,
-        itemBuilder: (context, index) => Text(meals[index].title),
+        itemBuilder: (context, index) => MealItem(
+          meal: meals[index],
+          onSelectMeal: () {
+            _selectCategory(context, meals[index]);
+          },
+        ),
       );
     }
 
